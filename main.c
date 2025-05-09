43154fe9d50447bc05afe60b306283216f6588e8
Coco <corentindubernet1@gmail.com>
2025-05-09 13:50:35 +0200

    attend

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "projet.h"
 
 const int LARGEUR_MAP = 162;
-const int SPAWN_X = 117;
-const int SPAWN_Y = 6;
+const int SPAWN_X = 21;
+const int SPAWN_Y = 15;
 const int SPAWN_MORT_X = 21;
 const int SPAWN_MORT_Y = 14;
-const int MORT_Y = 17;
+const int MORT_Y = 20;
 
-int main()
-{
+
+int main() {
     const char *fichierOriginal = "Mario.txt";
     menuPrincipal(fichierOriginal);
     return 0;
